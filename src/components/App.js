996e7ab4b1000fccc0612baec84875d86327e821
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-05-01 02:45:10 -0300

    feat: Using multipleproperties with the interpolation function

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function App() {
       <h1>App</h1>      
       <Button>Primary Button</Button>
       <Button secondary>Secondary Button</Button>
-      <Button disabled>Disabled Button</Button>
+      <Button disabled large>Disabled Button</Button>
     </>
   );
 }
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,19 +1,30 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Button = styled.button`
   color: white;
-  background: ${props => props.secondary ? 'black' : '#f8049c'};
-  font-weight:bold; 
-  padding: 8px;
-  border-radius: 4px;
+  background: ${(props) => (props.secondary ? 'black' : '#f8049c')};
+  font-weight: bold;
+  ${(p) =>
+    p.large
+      ? css`
+          padding: 10px;
+          border-radius: 5px;
+          font-size: 1.5rem;
+        `
+    : css`
+          padding: 8px;
+          border-radius: 4px;
+          font-size: 1rem;
+      `
+    }
+
   box-shadow: none;
-  font-size: 1rem;
   border: none;
   width: 100%;
   display: block;
   white-space: none;
 
-  &:disabled{
+  &:disabled {
     background: #eee;
     color: #666;
   }
