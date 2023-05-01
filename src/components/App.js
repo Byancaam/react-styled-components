edfb988247c3a1c8467124274bae4999e7fbc5a9
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-05-01 02:37:19 -0300

    feat: Creating different types of buttons using styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ function App() {
       <h1>App</h1>      
       <Button>Primary Button</Button>
       <Button secondary>Secondary Button</Button>
+      <Button disabled>Disabled Button</Button>
     </>
   );
 }
