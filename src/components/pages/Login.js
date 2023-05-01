c465bb164107ac0aeef8eb51e68b3a391f571ad6
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-05-01 15:14:05 -0300

    feat: Creating the login component

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,5 +1,10 @@
+import { PageLayout } from 'components/common';
 import React from 'react';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <PageLayout>
+      Login
+    </PageLayout>
+  );
 }
